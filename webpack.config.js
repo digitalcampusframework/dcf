01048ee80a39b27868471d386e8ade1afb0edc5d
Michael Fairchild <mfairchild365@gmail.com>
2018-01-03 15:50:28 -0600

    Make the theme configurable

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,41 @@
 const path = require('path');
 const webpack = require('webpack');
 
+
+
 // var styleLintPlugin = require('stylelint-webpack-plugin');
 var extractTextPlugin = require('extract-text-webpack-plugin');
 
-var baseConfig = {
-  entry: {
-    main: './theme/unl_5.0/js/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, './theme/unl_5.0/js'),
-    filename: 'debug.js'
-  },
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: extractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader','postcss-loader','sass-loader']
-        })
-      },
+var baseConfig = function(env) {
+
+    var theme = 'example';
+
+    if (env.theme) {
+        theme = env.theme;
+    }
+
+    return {
+        entry        : {
+            main : './theme/' + theme + '/js/index.js'
+        },
+        output       : {
+            path     : path.resolve(__dirname, './theme/unl_5.0/js'),
+            filename : 'debug.js'
+        },
+        watch        : true,
+        watchOptions : {
+            aggregateTimeout : 300,
+            poll             : 1000
+        },
+        module       : {
+            rules : [
+                {
+                    test : /\.scss$/,
+                    use  : extractTextPlugin.extract({
+                        fallback : 'style-loader',
+                        use      : ['css-loader', 'postcss-loader', 'sass-loader']
+                    })
+                },
 
 //       {
 //         test: /\.scss$/,
@@ -41,20 +51,20 @@ var baseConfig = {
 //         ]
 //       },
 
-    ]
-  },
-  plugins: [
+            ]
+        },
+        plugins      : [
 //     new styleLintPlugin({
 //       options: {
 //         syntax: 'scss'
 //       }
 //     }),
-    new extractTextPlugin({
-      filename: '../css/all.css',
-      disable: false,
-      allChunks: true
-    })
-  ]
+            new extractTextPlugin({
+                filename  : '../css/all.css',
+                disable   : false,
+                allChunks : true
+            })
+        ]
+    }
 };
-
 module.exports = baseConfig;
