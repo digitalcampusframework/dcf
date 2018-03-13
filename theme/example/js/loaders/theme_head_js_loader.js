ec541d730a8df4f3000144bcf3f3d79ef7ddbaff
Ryan Dee <ryan@skoolbus39.com>
2018-03-13 09:17:39 -0500

    Update JS, restore CSS output
    - Rename duplicate constants for ‘dcf_core_head’ and ‘dcf_core_body’
    - Restore ‘coreCSS’ constant which was inadvertently commented out
    - Import theme navigation component partial

--- theme/example/js/loaders/theme_head_js_loader.js
+++ theme/example/js/loaders/theme_head_js_loader.js
@@ -1,4 +1,4 @@
-const dcf_core = require('../../../../core/js/core-head.js');
+const dcf_core_head = require('../../../../core/js/core-head.js');
 
 // const debugCss = require('../scss/debug/debug.scss');
-// const coreCss = require('../../scss/all.scss');
+const coreCss = require('../../scss/all.scss');
