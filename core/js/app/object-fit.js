a9ca2200891a86a1d75f4a5f6d8f53606b308f40
Ryan Dee <ryan@skoolbus39.com>
2018-04-11 13:29:04 -0500

    Remove class type notation and BEM from JS

--- core/js/app/object-fit.js
+++ core/js/app/object-fit.js
@@ -1,12 +1,12 @@
 // Based on https://www.bmorecreativeinc.com/edge-object-fit-fallback-without-polyfill-modernizr/
 
-var results = document.querySelector( '.dcf-js-objfit-check' );
+var results = document.querySelector( '.dcf-objfit-check' );
 var objectfit = window.getComputedStyle( results, '::before' ).content;
 if( objectfit == 'none' ) {
   // Edge fallback
   // Javascript is nearly identical to the Medium article
-  $( '.dcf-u-objfit-cover' ).parent( 'div' ).addClass( 'dcf-u-objfit-cover-fallback' );
-  $( '.dcf-u-objfit-cover-fallback' ).each( function() {
+  $( '.dcf-objfit-cover' ).parent( 'div' ).addClass( 'dcf-objfit-cover-fallback' );
+  $( '.dcf-objfit-cover-fallback' ).each( function() {
     var $container = $( this ),
       imgUrl = $container.find( 'img' ).prop( 'src' );
     if( imgUrl ) {
