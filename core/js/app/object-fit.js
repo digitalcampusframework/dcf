af346c72937a6ae3b321c76384c9378c6059b2cb
Ryan Dee <ryan@skoolbus39.com>
2018-04-12 10:29:15 -0500

    Rename object-fit mixins/utilities

--- core/js/app/object-fit.js
+++ core/js/app/object-fit.js
@@ -1,12 +1,12 @@
 // Based on https://www.bmorecreativeinc.com/edge-object-fit-fallback-without-polyfill-modernizr/
 
-var results = document.querySelector( '.dcf-objfit-check' );
+var results = document.querySelector( '.dcf-obj-fit-check' );
 var objectfit = window.getComputedStyle( results, '::before' ).content;
 if( objectfit == 'none' ) {
   // Edge fallback
   // Javascript is nearly identical to the Medium article
-  $( '.dcf-objfit-cover' ).parent( 'div' ).addClass( 'dcf-objfit-cover-fallback' );
-  $( '.dcf-objfit-cover-fallback' ).each( function() {
+  $( '.dcf-obj-fit-cover' ).parent( 'div' ).addClass( 'dcf-obj-fit-cover-fallback' );
+  $( '.dcf-obj-fit-cover-fallback' ).each( function() {
     var $container = $( this ),
       imgUrl = $container.find( 'img' ).prop( 'src' );
     if( imgUrl ) {
