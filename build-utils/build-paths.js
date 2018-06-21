15e77f6e7a595df20b5a1cdd37284c9c37b35689
Richard Lock <lock.j.h@gmail.com>
2018-06-21 14:21:54 -0500

    Added object-fit and picture-fill package dependencies to the mustardJSGlob property inside build-paths.js

--- build-utils/build-paths.js
+++ build-utils/build-paths.js
@@ -7,13 +7,18 @@ const mustardJsSrcPath = path.join(commonPaths.srcPath, 'js', 'mustard');
 const appJsSrcPath = path.join(commonPaths.srcPath, 'js', 'app');
 const commonAppPath = path.join(commonPaths.outputBuild, 'js', 'app','common');
 
+//Load in dependencies from package
 const dialogPolyfill = require.resolve('dialog-polyfill');
+const picturefill = require.resolve('picturefill');
+const objectFitImages = require.resolve('object-fit-images');
 
 module.exports = {
 	vendorJsSrc: vendorJsSrcPath,
-	vendorJsGlob: [`${vendorJsSrcPath}/**/*.js`, `${dialogPolyfill}`], // better to use unminified vendor files since all files will be concatenated & minified
+	// better to use unminified vendor files since all files will be concatenated & minified
+	vendorJsGlob: [`${vendorJsSrcPath}/**/*.js`, `${dialogPolyfill}`],
 	vendorJsDest: path.join(commonPaths.outputBuild, 'js', 'vendor'),
-	mustardJsGlob: [`${mustardJsSrcPath}/**/*.js`], // better to use unminified vendor files since all files will be concatenated & minified
+	// better to use unminified vendor files since all files will be concatenated & minified
+	mustardJsGlob: [`${mustardJsSrcPath}/**/*.js`, `${picturefill}`, `${objectFitImages}`],
 	mustardJsDest: path.join(commonPaths.outputBuild, 'js', 'mustard'),
 	appJsPath: appJsSrcPath,
 	appJsGlob: `${appJsSrcPath}/**/*.js`,
