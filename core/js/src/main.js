0c4e8be67e36a8e4f5761455b9f05df199028578
Richard Lock <lock.j.h@gmail.com>
2018-06-26 11:48:02 -0500

    -Moved latest scss files from /core into /assets/src/scss -Rebuild dist folders -Removed /core

--- core/js/src/main.js
+++
@@ -1,9 +0,0 @@
-// create log function
-var log = function (data) {
-    if ("console" in window && "log" in console) {
-        console.log(data);
-    }
-};
-
-// load jquery
-var $ = require('../vendor/jquery.js');
