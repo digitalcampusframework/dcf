a1e54637704e5eb82f4b8bd7289bbfab19b24585
Ryan Dee <ryan@skoolbus39.com>
2019-07-29 15:38:01 -0500

    Temporarily remove dialog and notice widgets

--- example/js/src/main-body.js
+++ example/js/src/main-body.js
@@ -1,21 +1,21 @@
-let dialog = require('dcf-dialog');
+// let dialog = require('dcf-dialog');
 let LazyLoad = require('dcf-lazyLoad');
-let Notice = require('dcf-notice');
+// let Notice = require('dcf-notice');
 
 let flatpickr = require('flatpickr/flatpickr');
 require("flatpickr/flatpickr.css");
 
 
-const noticeOptions = {
-	widget: 'notice',
-	'notice-type': 'alert',
-	animation: 'true',
-	location: 'fixedBottom',
-	collapsible: 'true',
-	id: 'dynamicNotice1'
-};
-
-Notice.create('Spaghetti Monster Lives', 'You know no spaghetti', noticeOptions);
+// const noticeOptions = {
+// 	widget: 'notice',
+// 	'notice-type': 'alert',
+// 	animation: 'true',
+// 	location: 'fixedBottom',
+// 	collapsible: 'true',
+// 	id: 'dynamicNotice1'
+// };
+//
+// Notice.create('Spaghetti Monster Lives', 'You know no spaghetti', noticeOptions);
 
 
 let datepicker = document.querySelector('[data-widget*="flatpickr"]');
