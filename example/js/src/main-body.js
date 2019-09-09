59caf05a92a959288ade632ad100ab8a0fe40354
Ryan Dee <ryan@skoolbus39.com>
2019-09-09 13:17:09 -0500

    Group related JavaScript

--- example/js/src/main-body.js
+++ example/js/src/main-body.js
@@ -1,11 +1,26 @@
+// Lazy Load
 let LazyLoad = require('dcf-lazyLoad');
+const images = document.querySelectorAll('[loading=lazy], .dcf-lazy-load');
+const observerConfig = {
+	// extend intersection root margin by 50px to start intersection earlier by 50px
+	rootMargin: '0px 0px 50px 0px',
+	threshold: [0, 0.40]
+};
+const enterClassNames = ['dcf-fade-in'];
+const exampleLazyLoad = new LazyLoad(images, observerConfig, enterClassNames);
+exampleLazyLoad.initialize();
+
+
+// Modal
 let Modal = require('dcf-modal');
+const modals = document.querySelectorAll('.dcf-modal');
+const bodyScrollLock = require('body-scroll-lock');
+const exampleModal = new Modal(modals, bodyScrollLock);
+exampleModal.initialize();
+
+
+// Notice
 // let Notice = require('dcf-notice');
-
-// let flatpickr = require('flatpickr/flatpickr');
-// require("flatpickr/flatpickr.css");
-
-
 // const noticeOptions = {
 // 	widget: 'notice',
 // 	'notice-type': 'alert',
@@ -18,23 +33,11 @@ let Modal = require('dcf-modal');
 // Notice.create('Spaghetti Monster Lives', 'You know no spaghetti', noticeOptions);
 
 
+// Date Picker
+// let flatpickr = require('flatpickr/flatpickr');
+// require("flatpickr/flatpickr.css");
 // let datepicker = document.querySelector('[data-widget*="flatpickr"]');
 // flatpickr(datepicker, {
 // 	enableTime: true,
 // 	dateFormat: "Y-m-d h:iK",
 // });
-
-const images = document.querySelectorAll('[loading=lazy], .dcf-lazy-load');
-const observerConfig = {
-	// extend intersection root margin by 50px to start intersection earlier by 50px
-	rootMargin: '0px 0px 50px 0px',
-	threshold: [0, 0.40]
-};
-const enterClassNames = ['dcf-fade-in'];
-const exampleLazyLoad = new LazyLoad(images, observerConfig, enterClassNames);
-exampleLazyLoad.initialize();
-
-const modals = document.querySelectorAll('.dcf-modal');
-const bodyScrollLock = require('body-scroll-lock');
-const exampleModal = new Modal(modals, bodyScrollLock);
-exampleModal.initialize();
