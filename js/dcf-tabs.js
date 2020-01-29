6729f163a2c6760ab3d3f25ea1b740bef9f1cb5a
Peter Nguyen <petervnguyen99@gmail.com>
2020-01-29 16:30:15 -0600

    Fixed indent spacing issues, removed magic numbers, changed variable names that were too short, and converted nested ternary expressions to if else statements.

--- js/dcf-tabs.js
+++ js/dcf-tabs.js
@@ -1,98 +1,124 @@
-class DCFTabs{
-    constructor(tabGroups) {
-        this.tabGroups = tabGroups;
-    }
+class DCFTabs {
+  constructor(tabGroups) {
+    this.tabGroups = tabGroups;
+  }
 
-    initialize(){
-        Array.prototype.forEach.call(this.tabGroups, (tabGroup, i) => {
+  initialize() {
+    Array.prototype.forEach.call(this.tabGroups, (tabGroup) => {
+      // Define constants for tabs
+      const tabList = tabGroup.querySelector('.dcf-tabs > ol, .dcf-tabs > ul');
+      const tabs = tabList.querySelectorAll('a');
+      const panels = tabGroup.querySelectorAll('.dcf-tabs > div, .dcf-tabs > section');
+      const uuid = DCFUtility.uuidv4();
 
-            // Define constants for tabs
-            const tabList = tabGroup.querySelector('.dcf-tabs > ol, .dcf-tabs > ul');
-            const tabs = tabList.querySelectorAll('a');
-            const panels = tabGroup.querySelectorAll('.dcf-tabs > div, .dcf-tabs > section');
-            const uuid = DCFUtility.uuidv4();
+      // Prefix each tab group with a unique ID.
+      tabGroup.setAttribute('id', uuid.concat('-tab-group'));
 
-            // Prefix each tab group with a unique ID.
-            tabGroup.setAttribute('id', uuid + '-tab-group');
+      // Tab switching function
+      const switchTab = (oldTab, newTab) => {
+        newTab.focus();
+        // Make the active tab focusable by the user (Tab key)
+        newTab.removeAttribute('tabindex');
+        // Set the selected state
+        newTab.setAttribute('aria-selected', 'true');
+        oldTab.removeAttribute('aria-selected');
+        oldTab.setAttribute('tabindex', '-1');
+        // Get the indices of the new and old tabs to find the correct
+        // tab panels to show and hide
+        let index = Array.prototype.indexOf.call(tabs, newTab);
+        let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
+        panels[oldIndex].hidden = true;
+        panels[index].hidden = false;
+      };
 
-            // Tab switching function
-            const switchTab = (oldTab, newTab) => {
-                newTab.focus();
-                // Make the active tab focusable by the user (Tab key)
-                newTab.removeAttribute('tabindex');
-                // Set the selected state
-                newTab.setAttribute('aria-selected', 'true');
-                oldTab.removeAttribute('aria-selected');
-                oldTab.setAttribute('tabindex', '-1');
-                // Get the indices of the new and old tabs to find the correct
-                // tab panels to show and hide
-                let index = Array.prototype.indexOf.call(tabs, newTab);
-                let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
-                panels[oldIndex].hidden = true;
-                panels[index].hidden = false;
-            };
-
-            // Tab styling and functions.
-            Array.prototype.forEach.call(tabs, (tab, i) => {
-                // Add class to each tab
-                tab.classList.add('dcf-tab', 'dcf-d-block');
-                // Prefix each tab within its parent tab group with the corresponding uuid.
-                tab.setAttribute('id', uuid + '-tab-' + (i + 1));
-                // Add role to each tab
-                tab.setAttribute('role', 'tab');
-                // Add tabindex to each tab
-                tab.setAttribute('tabindex', '-1');
-                // Add class to each tab's parent (list item)
-                tab.parentNode.classList.add('dcf-tabs-list-item', 'dcf-mb-0');
-                // Add role to each tab's parent (list item)
-                tab.parentNode.setAttribute('role', 'presentation');
-                //Handle clicking of tabs for mouse users
-                tab.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    let currentTab = tabList.querySelector('[aria-selected]');
-                    if (e.currentTarget !== currentTab) {
-                        switchTab(currentTab, e.currentTarget);
-                    }
-                });
-                //Handle keydown events for keyboard users
-                tab.addEventListener('keydown', (e) => {
-                    // Get the index of the current tab in the tabs node list
-                    let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-                    // Work out which key the user is pressing and
-                    // Calculate the new tab's index where appropriate
-                    let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-                    if (dir !== null) {
-                        e.preventDefault();
-                        // If the down key is pressed, move focus to the open panel,
-                        // otherwise switch to the adjacent tab
-                        dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-                    }
-                });
-            });
-
-            // Add tab panel semantics and hide them all in each tab group.
-            Array.prototype.forEach.call(panels, (panel, i) => {
-                // Set role to each tab panel
-                panel.setAttribute('role', 'tabpanel');
-                // Set tabindex to let panel be focused
-                panel.setAttribute('tabindex', '-1');
-                // Add class to each tab panel
-                panel.classList.add('dcf-tabs-panel');
-                // Declare which tab labels each panel
-                panel.setAttribute('aria-labelledby', tabs[i].id);
-                panel.classList.add('dcf-mb-5');
-                // Hide all tab panels
-                panel.hidden = true;
-            });
-
-            // Add classes to tab list
-            tabList.classList.add('dcf-tabs-list', 'dcf-list-bare', 'dcf-mb-0');
-            // Add role to the tab list
-            tabList.setAttribute('role', 'tablist');
-            // Initially activate the first tab and reveal the first tab panel
-            tabs[0].removeAttribute('tabindex');
-            tabs[0].setAttribute('aria-selected', 'true');
-            panels[0].hidden = false;
+      // Tab styling and functions.
+      Array.prototype.forEach.call(tabs, (tab, tabIndex) => {
+        // Add class to each tab
+        tab.classList.add('dcf-tab', 'dcf-d-block');
+        // Prefix each tab within its parent tab group with the corresponding uuid.
+        let nextTab = tabIndex + DCFUtility.magicNumbers('int1');
+        tab.setAttribute('id', uuid.concat('-tab-', nextTab));
+        // Add role to each tab
+        tab.setAttribute('role', 'tab');
+        // Add tabindex to each tab
+        tab.setAttribute('tabindex', '-1');
+        // Add class to each tab's parent (list item)
+        tab.parentNode.classList.add('dcf-tabs-list-item', 'dcf-mb-0');
+        // Add role to each tab's parent (list item)
+        tab.parentNode.setAttribute('role', 'presentation');
+        // Handle clicking of tabs for mouse users
+        tab.addEventListener('click', (clickEvent) => {
+          clickEvent.preventDefault();
+          let currentTab = tabList.querySelector('[aria-selected]');
+          if (clickEvent.currentTarget !== currentTab) {
+            switchTab(currentTab, clickEvent.currentTarget);
+          }
         });
-    }
+        // Handle keydown events for keyboard users
+        tab.addEventListener('keydown', (keydownEvent) => {
+          // Get the index of the current tab in the tabs node list
+          let index = Array.prototype.indexOf.call(tabs, keydownEvent.currentTarget);
+          // Work out which key the user is pressing and
+          // Calculate the new tab's index where appropriate
+          const int37 = 37;
+          const int39 = 39;
+          const int40 = 40;
+          // let dir = keydownEvent.which === foo ? index - DCFUtility.magicNumbers('int1') : keydownEvent.which === foo1 ?
+          //   index + DCFUtility.magicNumbers('int1') : keydownEvent.which === foo2 ? 'down' : null;
+
+          let dir = 0;
+          if (keydownEvent.which === int37) {
+            dir = index - DCFUtility.magicNumbers('int1');
+          } else if (keydownEvent.which === int39) {
+            dir = index + DCFUtility.magicNumbers('int 1');
+          } else if (keydownEvent.which === int40) {
+            dir = 'down';
+          } else {
+            dir = null;
+          }
+
+
+          if (dir !== null) {
+            keydownEvent.preventDefault();
+            // If the down key is pressed, move focus to the open panel,
+            // otherwise switch to the adjacent tab
+            // dir === 'down' ? panels[tabIndex].focus() : tabs[dir] ?
+            //   switchTab(keydownEvent.currentTarget, tabs[dir]) : void DCFUtility.magicNumbers('int0');
+
+            if (dir === 'down') {
+              panels[tabIndex].focus();
+            } else if (tabs[dir]) {
+              switchTab(keydownEvent.currentTarget, tabs[dir]);
+            } else {
+              void DCFUtility.magicNumbers('int0');
+            }
+          }
+        });
+      });
+
+      // Add tab panel semantics and hide them all in each tab group.
+      Array.prototype.forEach.call(panels, (panel, panelIndex) => {
+        // Set role to each tab panel
+        panel.setAttribute('role', 'tabpanel');
+        // Set tabindex to let panel be focused
+        panel.setAttribute('tabindex', '-1');
+        // Add class to each tab panel
+        panel.classList.add('dcf-tabs-panel');
+        // Declare which tab labels each panel
+        panel.setAttribute('aria-labelledby', tabs[panelIndex].id);
+        panel.classList.add('dcf-mb-5');
+        // Hide all tab panels
+        panel.hidden = true;
+      });
+
+      // Add classes to tab list
+      tabList.classList.add('dcf-tabs-list', 'dcf-list-bare', 'dcf-mb-0');
+      // Add role to the tab list
+      tabList.setAttribute('role', 'tablist');
+      // Initially activate the first tab and reveal the first tab panel
+      tabs[DCFUtility.magicNumbers('int0')].removeAttribute('tabindex');
+      tabs[DCFUtility.magicNumbers('int0')].setAttribute('aria-selected', 'true');
+      panels[DCFUtility.magicNumbers('int0')].hidden = false;
+    });
+  }
 }
