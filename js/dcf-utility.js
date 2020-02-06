f57ac8084b4627cdb2e7e485216f13414cfc26f6
Jeff Sturek <jsturek8@unl.edu>
2020-02-06 16:10:45 -0600

    Add webp methods to DCFUtility

--- js/dcf-utility.js
+++ js/dcf-utility.js
@@ -3,6 +3,7 @@ class DCFUtility {
     const magicNumbers = {
       int0: 0,
       int1: 1,
+      int2: 2,
       int16: 16,
       hex0x3: 0x3,
       hex0x8: 0x8,
@@ -24,5 +25,37 @@ class DCFUtility {
         uuidv4 = uuid === 'x' ? rand : rand & HEX0x3 | HEX0x8;
       return uuidv4.toString(NUMERIC_16);
     });
+  }
+
+  static supportsWebp() {
+    let supports = false;
+    try {
+      const supportCheck = window.sessionStorage.getItem('webpSupport');
+      if (supportCheck !== null) {
+        supports = supportCheck;
+      } else {
+        let WebP = new Image();
+        WebP.src =
+          'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        WebP.onload = WebP.onerror = () => {
+          supports = WebP.height === DCFUtility.magicNumbers('int2');
+          window.sessionStorage.setItem('webpSupport', supports);
+        };
+      }
+    } catch (err) {
+      // do nothing
+    }
+
+    return supports;
+  }
+
+  static flagSupportsWebp(element = document.documentElement) {
+    if (DCFUtility.this.supportsWebp()) {
+      if (element.className.indexOf('dcf-no-webp') >= DCFUtility.magicNumbers('int0')) {
+        element.className = element.className.replace(/\bdcf-no-webp\b/, 'dcf-webp');
+      } else {
+        element.className.concat(' dcf-webp');
+      }
+    }
   }
 }
