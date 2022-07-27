1c29f1c64ec026a4f79f60e95360d7f2d078b2df
Thomas Neumann <thomasneumann21800@gmail.com>
2022-07-27 17:02:58 -0500

    changed gulp lintSCSS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 
 const { watch, src } = require('gulp');
 
 // Import Gulp plugins.
 const eslint = require('gulp-eslint');
-const scsslint = require('gulp-scss-lint');
+const stylelint = require('stylelint');
 const fs = require('fs');
 
 const config = {
@@ -21,13 +22,22 @@ function lintJS(cb) {
 }
 
 function lintSCSS(cb) {
-  src(config.scssSrc)
-    .pipe(scsslint({
-      'config': 'scssLint.yml',
-      'reporterOutput': 'scssLintReport.json'
-    }));
-
-  cb();
+  stylelint.lint({
+    files: config.scssSrc,
+    formatter: 'string', console: true
+  }).then((data) => { 
+    if (data.output) {
+      fs.writeFile('scssLintReport.txt', data.output, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+    cb();
+  }).catch((err) => {
+    console.error(err.stack);
+    cb(err);
+  });
 }
 
 exports.styles = lintSCSS;
