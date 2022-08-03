57745dbb4f3f04cb11e8b11fc4a8fb1cb1090753
Ryan Dee <ryan@skoolbus39.com>
2022-08-03 10:09:33 -0500

    Temporarily remove Sass linting

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,10 @@ const { watch, src } = require('gulp');
 
 // Import Gulp plugins.
 const eslint = require('gulp-eslint');
-const stylelint = require('stylelint');
 const fs = require('fs');
 
 const config = {
-  scssSrc:            './scss/**/*.scss',
+  // scssSrc:            './scss/**/*.scss',
   jsSrc:              './js/**/*.js'
 };
 
@@ -21,30 +20,11 @@ function lintJS(cb) {
   cb();
 }
 
-function lintSCSS(cb) {
-  stylelint.lint({
-    files: config.scssSrc,
-    formatter: 'string', console: true
-  }).then((data) => { 
-    if (data.output) {
-      fs.writeFile('scssLintReport.txt', data.output, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-    cb();
-  }).catch((err) => {
-    console.error(err.stack);
-    cb(err);
-  });
-}
-
-exports.styles = lintSCSS;
+// exports.styles = lintSCSS;
 exports.scripts = lintJS;
 exports.default = function() {
   // Watch SCSS and lint if changes occur
-  watch(config.scssSrc, { ignoreInitial: false }, exports.styles);
+  // watch(config.scssSrc, { ignoreInitial: false }, exports.styles);
 
   // Watch JS and process if changes occur
   watch(config.jsSrc, { ignoreInitial: false }, exports.scripts);
